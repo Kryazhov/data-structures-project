fefbb6fea56e449e365e8c4399f98fd1d54488e3
Kryazhov Roman <kryazhov.roman@mail.ru>
2023-09-08 01:57:01 +0600

    homework-1

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack
+
+class StackTestCase(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_push(self):
+        self.assertIsNone(self.stack.top)
+
+        self.stack.push(1)
+        self.assertEqual(self.stack.top.data, 1)
+        self.assertIsNone(self.stack.top.next_node)
+
+        self.stack.push(2)
+        self.assertEqual(self.stack.top.data, 2)
+        self.assertEqual(self.stack.top.next_node.data, 1)
